8c68a5297e456a1f39d3128259d01973dfdd06c8
ihebchagra <ihebchagra@gmail.com>
2019-12-06 11:35:43 +0100

    changed threshhold to 6 seconds

--- .local/bin/mpcprevious.sh
+++ .local/bin/mpcprevious.sh
@@ -2,7 +2,7 @@
 time=$(mpc | sed -n 2p | awk '{print $3}' | sed 's/\/.*//')
 min=$(echo $time | sed 's/\:.*//' )
 sec=$(echo $time | sed 's/\.*://' )
-if [ $min -eq 0 ] && [ $sec -lt 10 ]
+if [ $min -eq 0 ] && [ $sec -lt 6 ]
 then mpc -q prev
 else mpc -q seek 0
 fi
